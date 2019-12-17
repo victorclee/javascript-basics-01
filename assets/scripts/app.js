8a51e665b1cf99c2838256466db6fb3bd1a0ad6a
victorclee <victorchristopherlee@gmail.com>
2019-12-17 16:44:31 -0600

    simple add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,13 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1 ;
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
 
-let calculationDescription = ('(' + currentResult + ' + 10) * 3 / 2 - 1');
+currentResult = add(1, 2);
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
