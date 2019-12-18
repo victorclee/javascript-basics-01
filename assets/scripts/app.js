490b36c96a3ae3c6146a79913d254fd13bce8ce0
victorclee <victorchristopherlee@gmail.com>
2019-12-18 14:48:54 -0600

    add function now updates results.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
+addBtn.addEventListener('click', add);
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
