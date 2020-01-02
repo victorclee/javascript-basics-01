b8c317369a40ceaa77ba86c800d9b4809a63f1b0
victorclee <victorchristopherlee@gmail.com>
2020-01-02 15:31:59 -0600

    truthy falsey value

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,15 +30,17 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
   if (
     calculationType !== 'ADD' &&
     calculationType !== 'SUBTRACT' &&
     calculationType !== 'MULTIPLY' &&
-    calculationType !== 'DIVIDE'
+    calculationType !== 'DIVIDE' ||
+    !enteredNumber
   ) {
     return;
-  }  
-  const enteredNumber = getUserNumberInput();
+  } 
+   
   const initialResult = currentResult;
   let mathOperator;
   if (calculationType === 'ADD') {
